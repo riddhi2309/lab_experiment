e1fe4b60f7276b0d76397cdeb698c06b953945cf
riddhi2309 <141670096+riddhi2309@users.noreply.github.com>
2023-09-08 21:21:03 +0530

    Update q_04.py

--- q_04.py
+++ q_04.py
@@ -1,10 +1,8 @@
 x = int(input("Enter the three digit number:-"))
-first_digit= int(int(x)[0])
-tenth_digit = int(int(x)[1])
-hunderth_digit = int(int(x)[2])
-sum  = first_digit + tenth_digit+ hunderth_digit
-print(sum)
-if sum%3 == 0:
-    print ("The number is divisible by 3" , sum)
-else:
-    print("The number is not divisible by 3" , sum)
+a=0
+b=0
+while x!=0:
+    a=x%10
+    b=b+a
+    x=x//10
+print(b)
